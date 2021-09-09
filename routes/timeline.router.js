58766cefc196813160d3abd3d4d72f47cc59e482
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-09-09 22:50:18 +0530

    refactor

--- routes/timeline.router.js
+++ routes/timeline.router.js
@@ -1,21 +1,35 @@
 const express = require("express");
+const cors = require("cors");
 const { authVerify } = require("../middlewares/authVerify");
 const { Post } = require("../models/post.model");
+const { headers } = require("../middlewares/headers");
 const { User } = require("../models/user.model");
 const router = express.Router();
 
-router.route("/").get(authVerify, async (req, res) => {
-  try {
-    const allPosts = await Post.find({}).populate(
-      "user",
-      "name username profilePicture _id"
-    );
-    allPosts.reverse();
+router.use("/", authVerify);
+router.route("/").get(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }),
+  headers,
+  async (req, res) => {
+    try {
+      const allPosts = await Post.find({}).populate(
+        "user",
+        "name username profilePicture _id"
+      );
+      allPosts.reverse();
 
-    res.status(200).json({ success: true, message: "Getting Posts", allPosts });
-  } catch (err) {
-    res.status(404).json({ success: false, message: "posts not found" });
+      res
+        .status(200)
+        .json({ success: true, message: "Getting Posts", allPosts });
+    } catch (err) {
+      res.status(404).json({ success: false, message: "posts not found" });
+    }
   }
-});
+);
 
 module.exports = { router };
