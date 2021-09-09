69d569db069cf66059863efbc486eb25eab8d926
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-09-09 23:44:15 +0530

    refactor

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ startConnection();
 app.use(
   cors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     preflightContinue: false,
     optionsSuccessStatus: 204,
   })
